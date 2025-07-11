377b6df7557483d8467dd0ec7ae5c4e66ce05f84
AthfanFasee <aththaar47@gmail.com>
2025-07-11 12:59:22 +0800

    make each test run on a random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::net::TcpListener;
 use moodfeed::run;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    run("127.0.0.1:8000")?.await
+async fn main() {
+    if let Err(e) = try_main().await {
+        eprintln!("Server startup error: {}", e);
+    }
+}
+
+async fn try_main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,14 @@
+use std::net::TcpListener;
+
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -14,9 +17,16 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = moodfeed::run("127.0.0.1:0").expect("Failed to start app.");
+fn spawn_app()-> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // retrieve the port assigned by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = moodfeed::run(listener).expect("Failed to bind server address");
 
     let _ = tokio::spawn(server);
+    
+    format!("http://127.0.0.1:{}", port)
 }
 
